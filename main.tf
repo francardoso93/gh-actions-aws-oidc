52efada39af960088568525d7fc1cf6622d63705
Francisco Cardoso <francardoso@outlook.com>
2022-03-30 11:16:04 -0300

    Working

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = var.org_repo_allow_list // example: ["repo:org1/*:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy" "AdministratorAccess" {
+  name = "AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = data.aws_iam_policy.AdministratorAccess.arn
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = string
+}
+
+variable "profile" {
+  type = string
+}
+
+
+variable "org_repo_allow_list" {
+  type = list(string)
+}
+
+variable "role_name" {
+  type = string
+}
